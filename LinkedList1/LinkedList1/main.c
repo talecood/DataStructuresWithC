1b0edc3e0c281d1e7cb38f80d37101f4fa6b0284
talecood <goktug.tabakk@gmail.com>
2024-08-15 10:23:55 +0300

    Basic LinkedList Created With Struct and Pointers

--- LinkedList1/LinkedList1/main.c
+++ LinkedList1/LinkedList1/main.c
@@ -1,10 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
 
-int main(void) {
+struct n {
+    int x;
+    struct n* next;
+};
+typedef struct n node;
 
-	return 0;
+int main(void) {
+    node* root;
+
+    root = (node*)malloc(sizeof(node));
+
+        
+    root->x = 10;
+    root->next = (node*)malloc(sizeof(node));
+    root->next -> x = 20;
+    root->next->next = (node*)malloc(sizeof(node));
+
+
+    root->next->next->x = 30;
+
+    node* iterator;
+    iterator = root;
+
+    printf("%d ", iterator->x);
+    iterator = iterator->next;
+
+    printf("%d", iterator->x);
+
+    
+
+
+    return 0;
 }
