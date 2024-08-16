dc5ad96fe8e0b65b28cf8746b31cb6e5ae795d48
talecood <goktug.tabakk@gmail.com>
2024-08-16 11:30:57 +0300

    pop fonksiyonu tamamlandı

--- StackWithLinkedList/StackWithLinkedList/stack_ll.c
+++ StackWithLinkedList/StackWithLinkedList/stack_ll.c
@@ -3,7 +3,42 @@
 #include <stdlib.h>
 #include "stack_ll.h"
 
-int pop(node* r) {
+int pop(node* root) {
+	if (root == NULL) {
+		printf("Stack is Empty.");
+		return -1;
+	}
+	
+	node* iterator = root;
+	while (iterator->next->next != NULL) {
+		iterator = iterator->next;
+	}
+	node* temp = iterator->next;
+	int rvalue = temp->data;
+
+	free(temp);
+	iterator->next = NULL;
+	return root;
+}
+
+void push(node* root, int a) {
+	if (root == NULL) {
+		root = (node*)malloc(sizeof(node));
+		root->data = a;
+		root->next = NULL;
+		return root;
+	}
+	
+	node* iterator = root;
+	while (iterator->next != NULL) {
+		iterator = iterator->next;
+	}
+	node* temp = (node*)malloc(sizeof(node));
+	temp->data = a;
+	temp->next = NULL; 
+	iterator->next = temp;
+	return root;
+
 
 }
 
--- StackWithLinkedList/StackWithLinkedList/stack_ll.h
+++ StackWithLinkedList/StackWithLinkedList/stack_ll.h
@@ -1,1 +1,13 @@
-#pragma once
+#ifndef stack_ll
+#define stack_ll
+
+struct n {
+	node* next;
+	int data;
+}; typedef struct n node;
+
+int pop(node*);
+void push(node*, int);
+
+
+#endif
