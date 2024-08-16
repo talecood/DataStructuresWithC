9757f20d8a211ece090a303a8b15e04e38181fa8
talecood <goktug.tabakk@gmail.com>
2024-08-16 09:20:01 +0300

    append,appendByOrder and Delete Func. For Circular Linked List

--- CircularLinkedList/CircularLinkedList/main.c
+++ CircularLinkedList/CircularLinkedList/main.c
@@ -15,6 +15,7 @@ typedef struct n node;  // Create a typedef for easier usage
 // Function to print all elements in the linked list
 void print(node* r) {
     node* iterator = r;
+    printf("%d", iterator->x);
     while (iterator != r) {  // Loop until the end of the list
         printf("%d -> ", iterator->x);  // Print the value of the current node
         iterator = iterator->next;  // Move to the next node
@@ -24,25 +25,32 @@ void print(node* r) {
 
 // Function to append a new node with value x at the end of the list
 void append(node* r, int x) {
-    while (r->next != NULL) {  // Loop to find the end of the list
-        r = r->next;
+    node* iterator = r;
+    while (iterator->next != r) {  // Loop to find the end of the list
+        iterator = iterator->next;
     }
-    r->next = (node*)malloc(sizeof(node));  // Allocate memory for the new node
-    r->next->x = x;  // Set the value of the new node
-    r->next->next = NULL;  // Set the next pointer of the new node to NULL
+
+    iterator->next = (node*)malloc(sizeof(node));  // Allocate memory for the new node
+    iterator->next->x = x;  // Set the value of the new node
+    iterator->next->next = r;  // Set the next pointer of the new node to Root
 }
 
 // Function to insert a new node with value x in the correct order
 node* appendByOrder(node* r, int x) {
     if (r == NULL) {  // If the list is empty
         r = (node*)malloc(sizeof(node));  // Allocate memory for the new node
-        r->next = NULL;  // Set the next pointer to NULL
+        r->next = r;  // Set the next pointer to NULL
         r->x = x;  // Set the value of the new node
     }
     if (r->x > x) {  // If the new value should be inserted before the head
         node* temp = (node*)malloc(sizeof(node));  // Allocate memory for the new node
         temp->x = x;  // Set the value of the new node
         temp->next = r;  // Set the next pointer to the current head
+        node* iterator = r;
+        while (iterator->next != r) {
+            iterator = iterator->next;
+        }
+        iterator->next = temp;
         return temp;  // Return the new node as the new head
     }
     node* iter = r;  // Initialize iterator to traverse the list
@@ -61,68 +69,45 @@ node* delete(node* r, int x) {
     node* temp;  // Pointer to hold the node to be deleted
     node* iterator = r;  // Pointer to traverse the list
 
-    // If the node to be deleted is the head of the list
     if (r->x == x) {
-        temp = r;  // Save the current head node to temp
-        r = r->next;  // Move the head pointer to the next node
-        free(temp);  // Free the memory of the old head node
-        return r;  // Return the new head of the list
+        while (iterator->next != r) {
+            iterator = iterator->next;
+        }
+        iterator->next = r->next;
+        free(r);
+        return iterator->next;
     }
-
-    // Traverse the list to find the node to be deleted
-    while (iterator->next != NULL && iterator->next->x != x) {
-        iterator = iterator->next;  // Move to the next node
+    while (iterator->next != r && iterator->next->x != x) {
+        iterator = iterator->next;
     }
-
-    // If the node to be deleted is not found
-    if (iterator->next == NULL) {
-        printf("Number not found.\n");  // Print an error message
-        return r;  // Return the unchanged list
+    if (iterator->next == r) {
+        printf("Value Not Found.");
+        return r;
     }
-
-    // Node to be deleted is found, remove it from the list
-    temp = iterator->next;  // Save the node to be deleted
-    iterator->next = iterator->next->next;  // Bypass the node to be deleted
-    free(temp);  // Free the memory of the node to be deleted
-
-    return r;  // Return the head of the list
+    temp = iterator->next;
+    iterator->next = iterator->next->next;
+    free(temp);
+    return r;
 }
 
 
 int main(void) {
-    node* root;  // Pointer to the head of the list
-    root = (node*)malloc(sizeof(node));  // Allocate memory for the head node
-    root = NULL;  // Initialize the list as empty
-    node* iterator;  // Pointer to traverse the list
-    iterator = root;
-
-    // Insert nodes into the list in order
+    node* root;
+    root = NULL;
     root = appendByOrder(root, 400);
     root = appendByOrder(root, 40);
     root = appendByOrder(root, 4);
     root = appendByOrder(root, 450);
     root = appendByOrder(root, 50);
-
-    // Print all the nodes in the list
-    print(root);
-
+    bastir(root);
     root = delete(root, 50);
+    print(root);
     root = delete(root, 999);
+    print(root);
     root = delete(root, 4);
+    print(root);
     root = delete(root, 450);
     print(root);
-
-    // Add a new node with value 100 between nodes
-    for (int i = 0; i < 3; i++) {
-        iterator = iterator->next;  // Move to the desired position
-    }
-    node* temp = (node*)malloc(sizeof(node));  // Allocate memory for the new node
-    temp->next = iterator->next;  // Set the next pointer of the new node
-    iterator->next = temp;  // Insert the new node into the list
-    temp->x = 100;  // Set the value of the new node
-
-    printf("\nNew LinkedList : \n");
-    print(root);  // Print the updated list
 
     return 0;
 }
