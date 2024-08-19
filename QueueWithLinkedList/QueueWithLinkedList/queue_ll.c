2e6451da76583b70fd427f2f8380f85215614e6a
talecood <goktug.tabakk@gmail.com>
2024-08-19 10:21:54 +0300

    Queue With Linked List enqueue() ve dequeue()

--- QueueWithLinkedList/QueueWithLinkedList/queue_ll.c
+++ QueueWithLinkedList/QueueWithLinkedList/queue_ll.c
@@ -2,17 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct {
-	int x;
+struct n{
+	int data;
 	node* next;
 };
 typedef struct n node;
 
+node* root = NULL;
 void enqueue(int a) {
+	if (root == NULL) {
+		root = (node*)malloc(sizeof(node));
+		root->data = a;
+		root->next = NULL;
 
+	}
 }
 
 int dequeue() {
+	if (root == NULL) {
+		printf("Queue is empty.");
+		return -1;
+	}
+	int rvalue = root->data;
+	node* temp = root;
+	root = root->next;
+	free(temp);
 
 }
 
